cc6c22abd587a80fc18423bd43a4b49d08f71829
Ana Harris <vana1990@gmail.com>
2018-11-29 14:50:04 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -22,18 +22,21 @@ function addNewToy(event) {
   event.preventDefault()
   let name = document.querySelector('.input-name').value
   let url = document.querySelector('.input-url').value
-  postFetch(name, url)
-}
-
-function postFetch(name, url) {
   let data = {
     name: name,
-    image: url
+    image: url,
+    likes: 0
   }
+  // renderToy(data) => optimistic, then fetch
+  postFetch(name, url) // => pessimistic, then render
+}
+
+function postFetch(data) {
   fetch('http://localhost:3000/toys', {
     method: 'POST',
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json", // => the type of information it's sending
+      Accept: "application/json" // => the type of information it's going to send back
     },
     body: JSON.stringify(data)
   })
@@ -49,10 +52,25 @@ function fetchToys() {
     })
 }
 
-function addLike(event){
-  let btn = event.currentTarget
-  btn.likes += 1
-  btn.previousElementSibling.innerText = `Likes: ${btn.likes}`
+function patchFetch(e, id, likes) {
+  fetch(`http://localhost:3000/toys/${id}`, {
+    method: 'PATCH',
+    headers:
+    {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({likes: likes + 1}),
+  })
+    .then(res => res.json())
+    .then(data => {
+      e.target.parentElement.querySelector('p').innerText = `${data.likes} Likes`
+      e.target.likes = data.likes
+    })
+}
+
+function addLike(e) {
+  patchFetch(e, e.target.id, parseInt(e.currentTarget.likes))
 }
 
 function renderToy(toy) {
@@ -67,14 +85,16 @@ function renderToy(toy) {
   img.src = toy.image
 
   let p = document.createElement('p')
+  p.innerText = `${toy.likes} Likes`
 
   let button = document.createElement('button')
   button.classList.add('like-btn')
   button.innerText = 'Like <3'
-  button.likes = 0
+  button.id = toy.id
+  button.likes = toy.likes
   button.addEventListener('click', addLike)
 
-  divEl.appendChild(nameH)
+  // divEl.append(nameH, img, p, button)
   divEl.appendChild(nameH)
   divEl.appendChild(img)
   divEl.appendChild(p)
